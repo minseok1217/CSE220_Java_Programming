2926d045c22a49ab22beb9be66c2b2fdad40784b
minseok1217 <ping5432121@gmail.com>
2023-11-15 01:01:35 +0900

    Environmental Construction javaFx

--- Pokemon/Pokemon/src/App.java
+++ Pokemon/Pokemon/src/App.java
@@ -6,11 +6,11 @@ import javafx.stage.Stage;
 public class App extends Application{
     @Override
     public void start(Stage primarStage) throws Exception{
-        Label myLabel =new Label("Hello world");
+        // Label myLabel =new Label("Hello world");
         StackPane pane = new StackPane();
-        pane.getChildren().add(myLabel);
-        primarStage.setTitle("Simple JavaFX app");
-        primarStage.setScene((new Scene(pane,300,200)));
+        // pane.getChildren().add(myLabel);
+        primarStage.setTitle("PokeMon");
+        primarStage.setScene((new Scene(pane,600,500)));
         primarStage.show();
     }
     public static void main(String[] args) {
---
+++ Pokemon/Pokemon/src/User.java
@@ -0,0 +1,6 @@
+import java.io.IOException;
+
+public class User {
+    private String user_name;
+    
+}
